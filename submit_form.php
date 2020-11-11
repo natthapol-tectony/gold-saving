9cdbeb0acab005bbf91f918f60ee7646513dd09e
natthapol-tectony <72802836+natthapol-tectony@users.noreply.github.com>
2020-11-11 10:37:30 +0700

    Delete submit_form.php

--- submit_form.php
+++
@@ -1,9 +0,0 @@
-<form name="yourform" id="yourform" method="POST" action="yourpage.html">
-    <input type=hidden name="data" value="yourdata">
-    <input type="submit" id="send" name="send" value="Send">
-</form>
-<script>            
-    document.addEventListener("DOMContentLoaded", function(event) {
-            document.createElement('form').submit.call(document.getElementById('yourform'));
-            });         
-</script>
